faea969d75f004d3861398292da1be44121a2450
yinheyi <chinayinheyi@163.com>
2019-04-11 23:27:42 +0800

    提示一部分19代码，实在是困了, 我坚持不下去了

---
+++ 19_正则表达式的匹配.cpp
@@ -0,0 +1,68 @@
+/***********************************************************************
+*   Copyright (C) 2019  Yinheyi. <chinayinheyi@163.com>
+*   
+* This program is free software; you can redistribute it and/or modify it under the terms
+* of the GNU General Public License as published by the Free Software Foundation; either 
+* version 2 of the License, or (at your option) any later version.
+
+*   Brief:    
+*   Author: yinheyi
+*   Email: chinayinheyi@163.com
+*   Version: 1.0
+*   Created Time: 2019年04月11日 星期四 23时03分43秒
+*	Modifed Time: 2019年04月11日 星期四 23时26分58秒
+*   Blog: http://www.cnblogs.com/yinheyi
+*   Github: https://github.com/yinheyi
+*   
+***********************************************************************/
+
+
+// 题目：实现一个函数来匹配 . 和 *,  . 匹配任意字符， * 匹配任意个它前面的字符
+// 
+// 我的想法：
+// 首先对正则表达式进行划分，再从头开始每一部分去匹配字符串。
+// 使用多个指针来完成
+bool Match1(char* pStr_, char* pPattern_)
+{
+	// 参数的有效性检测
+	if (!pStr_ || !pPattern_)
+		return false;
+
+	// 都为空时，返回true
+	if (!*pStr_ && !*pPattern_)
+		return true;
+
+	// 当正则表达式为空，但是字符串不为空时，返回false
+	if (*pStr && !*pPattern_)
+		return false;
+
+	// 使用两个指针在正则表达式上移动，使用一个指针在字符串的移动
+	//
+	//
+	// 好吧，我想了想，好多情况，我还是放弃了，直接看答案了
+}
+
+
+// 原书答案
+// 的确好！！！！
+//
+bool match(char* str, char* pattern)
+{
+	if (str == nullptr && pattern == nullptr)
+		return false;
+
+	return mathCore(str, pattern);
+}
+
+bool matchCore(char* str, char* pattern)
+{
+	if (*str == '\0' && *pattern == '\0')
+		return true;
+
+	if (*str != '\0' && *pattern == '\0')
+		return false;
+
+	if (
+}
+
+
