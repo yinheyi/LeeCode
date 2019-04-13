88f80bc5114d2e1ca034e411e49052002d342baa
yinheyi <chinayinheyi@163.com>
2019-04-13 23:01:32 +0800

    按答题修改了代码，变得容易扩展

--- 21_调整数组的顺序.cpp
+++ 21_调整数组的顺序.cpp
@@ -10,7 +10,7 @@
 *   Email: chinayinheyi@163.com
 *   Version: 1.0
 *   Created Time: 2019年04月13日 星期六 21时53分52秒
-*	Modifed Time: 2019年04月13日 星期六 22时42分27秒
+*	Modifed Time: 2019年04月13日 星期六 22时59分54秒
 *   Blog: http://www.cnblogs.com/yinheyi
 *   Github: https://github.com/yinheyi
 *   
@@ -20,9 +20,15 @@
 // 题目： 调整一个数组的顺序，使其奇数在前，偶数在后
 //
 //
+static bool IsOdd(int nNumber_)
+{
+	return nNumber_ & 0x1; 
+}
+
+typedef bool (*FuncPtr)(int);
 // 参数说明： 
-// pArray_:指向数组首元素的指针; nLength_: 数组的长度
-static bool AdjustArray(int* pArray_, int nLength_)
+// pArray_:指向数组首元素的指针; nLength_: 数组的长度; pFunc, 函数指针
+static bool AdjustArray(int* pArray_, int nLength_, FuncPtr pFunc_)
 {
 	// 参数的合法性分析
 	if (pArray_ == nullptr)
@@ -38,11 +44,11 @@ static bool AdjustArray(int* pArray_, int nLength_)
 	while (true)
 	{
 		// 从前往后找到第一个偶数
-		while (pArray_[_nStart] & 0x1 && _nStart < _nEnd)
+		while (pFunc_(pArray_[_nStart]) && _nStart < _nEnd)
 			++_nStart;
 
 		// 从后向前找到第一个奇数
-		while (!(pArray_[_nEnd] & 0x1) && _nEnd > _nStart)
+		while (!pFunc_(pArray_[_nEnd]) && _nEnd > _nStart)
 			--_nEnd;
 
 		// 判断
@@ -68,20 +74,20 @@ int main()
 	int _nTest4[] = {2, 1};
 
 	std::cout << "第一个结果：" << std::endl;
-	std::cout << AdjustArray(_nTest1, 20) << std::endl;
+	std::cout << AdjustArray(_nTest1, 20, IsOdd) << std::endl;
 	for (int i = 0; i < 20; ++i)
 		std::cout << _nTest1[i] << std::endl;
 
 	std::cout << "第二个结果：" << std::endl;
-	std::cout << AdjustArray(_nTest2, 0) << std::endl;
+	std::cout << AdjustArray(_nTest2, 0, IsOdd) << std::endl;
 
 	std::cout << "第三个结果：" << std::endl;
-	std::cout << AdjustArray(_nTest3, 1) << std::endl;
+	std::cout << AdjustArray(_nTest3, 1, IsOdd) << std::endl;
 	for (int i = 0; i < 1; ++i)
 		std::cout << _nTest3[i] << std::endl;
 
 	std::cout << "第四个结果：" << std::endl;
-	std::cout << AdjustArray(_nTest4, 2) << std::endl;
+	std::cout << AdjustArray(_nTest4, 2, IsOdd) << std::endl;
 	for (int i = 0; i < 2; ++i)
 		std::cout << _nTest4[i] << std::endl;
 
