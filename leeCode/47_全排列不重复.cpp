9335ec9386296bf42b8e52b5cd7fcc527921dbd2
yinheyi <chinayinheyi@163.com>
2020-06-24 00:58:00 +0800

    以前做过，不难。

---
+++ leeCode/47_全排列不重复.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        searchFlag.resize(nums.size(), false);
+        DFS(nums);
+        return outputs;
+    }
+
+private:
+    void DFS(const vector<int>& nums)
+    {
+        bool allsearched = true;
+        set<int> s;
+        for (size_t i = 0; i < searchFlag.size(); ++i) {
+            if (!searchFlag[i] && s.count(nums[i]) == 0) {
+                allsearched = false;
+                s.insert(nums[i]);
+
+                current.push_back(nums[i]);
+                searchFlag[i] = true;
+
+                DFS(nums);
+
+                current.pop_back();
+                searchFlag[i] = false;
+            }
+        }
+        if (allsearched) {
+            outputs.push_back(current);
+        }
+    }
+
+    vector<vector<int>> outputs;
+    vector<int> current;
+    vector<bool> searchFlag;
+};
