77805554f477da3d419adfcd55cefba7ee702668
yinheyi <chinayinheyi@163.com>
2020-06-28 00:38:06 +0800

    该方法真不错.

--- leeCode/60_第k个排序.cpp
+++ leeCode/60_第k个排序.cpp
@@ -6,48 +6,44 @@ using namespace std;
 
 class Solution {
 public:
-    string getPermutation(int n, int k) {
+    string getPermutation(int n, int k)
+    {
+        k -= 1;
+        string result;
         string str;
         for (int i = 1; i <= n; ++i) {
             str.push_back('0' + i);
         }
-        DFS(str, k);
+
+        while (str.size() > 1) {
+            int factor = factorial(str.size() - 1);
+            int index = k / factor;
+            k %= factor;
+            result.push_back(str[index]);
+            str.erase(index, 1);
+        }
+        result.push_back(str.back());
         return result;
     }
 
-    void DFS(string& input, int k)
-    {
-        if (count >= k) {
-            return;
-        }
-
-        if (curStr.size() == input.size()) {
-            ++count;
-            result = curStr;
-        }
-
-        for (size_t i = 0; i < input.size(); ++i) {
-            if (input[i] != '#') {
-                curStr.push_back(input[i]);
-                input[i] = '#';
-                DFS(input, k);
-                input[i] = curStr.back();
-                curStr.pop_back();
-            }
-        }
-    }
-
 private:
-    int count = 0;
-    string result;
-    string curStr;
+    int factorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; ++i) {
+            result *= i;
+        }
+        return result;
+    }
 };
 
 
 int main()
 {
+    int i, j;
     Solution solver;
-    string result = solver.getPermutation(1, 23232434);
-    cout << result << endl;
+    while (cin >> i >> j) {
+        string result = solver.getPermutation(i, j);
+        cout << result << endl;
+    }
     return 0;
 }
