d703c77382f81e869dde157d4cb0e1df11ca6b98
yinheyi <chinayinheyi@163.com>
2020-06-29 00:12:25 +0800

    想了一天,这还是中等题, 怀疑自己太笨了.

---
+++ leeCode/209_长度最小的子数组.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+
+        const int length = nums.size();
+        int first = 0;
+        int second = 0;
+        int minLengh = 0x7fffffff;
+        int currentSum = 0;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum < s) {
+            return 0;
+        }
+
+        while (second < length) {
+            while (currentSum < s && second < length) {
+                currentSum += nums[second++];
+            }
+            while (currentSum >= s && first < second) {
+                currentSum -= nums[first++];
+            }
+            minLengh = min(minLengh, second - first + 1);
+        }
+        return minLengh == 0x7fffffff ? 0 : minLengh;
+    }
+};
+
+int main()
+{
+    Solution solver;
+    vector<int> inputs{2, 3, 1, 2, 4, 3};
+    cout << solver.minSubArrayLen(7, inputs) << endl;
+    return 0;
+}
