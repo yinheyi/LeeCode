91475698f255bce068f287f73d9d8697244a2084
yinheyi <chinayinheyi@163.com>
2020-07-11 16:21:20 +0800

    简单题.

--- leeCode/73_矩阵置零.cpp
+++ leeCode/73_矩阵置零.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        
+        if (matrix.empty()) {
+            return;
+        }
+        vector<size_t> rowZero;
+        vector<size_t> columnZero;
+        const size_t rows = matrix.size();
+        const size_t columns = matrix.back().size();
+        for (size_t i = 0; i < rows; ++i) {
+            for (size_t j = 0; j < columns; ++j) {
+                if (matrix[i][j] == 0) {
+                    rowZero.push_back(i);
+                    columnZero.push_back(j);
+                }
+            }
+        }
+
+        for (size_t i = 0; i < rowZero.size(); ++i) {
+            for (size_t j = 0; j < columns; ++j) {
+                matrix[rowZero[i]][j] = 0;
+            }
+        }
+
+        for (size_t i = 0; i < columnZero.size(); ++i) {
+            for (size_t j = 0; j < rows; ++j) {
+                matrix[j][columnZero[i]] = 0;
+            }
+        }
     }
 };
